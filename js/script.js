cb107b80602277b3a83f1c76f8379d8cecd91be5
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2021-01-30 14:56:16 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 /* Custom JS */
+$(document).ready(function() {
+	function setMenuHeight() {
+		var navbarHeight = $(".navbar").outerHeight();
+		$(".menu-wrapper")
+		.outerHeight(document.documentElement.clientHeight - navbarHeight)
+		.niceScroll({
+			emulatetouch: true
+		});
+	}
+	setMenuHeight();
+	$(window).on("resize", function(){
+		setMenuHeight();
+	})
+})
